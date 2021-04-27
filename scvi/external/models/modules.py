84e1eb659baa11badc58cb09dca06ce668a35269
Khalil Ouardini <khalil.ouardini@student.ecp.fr>
2021-04-27 01:49:46 -0700

    Simplifying modules

--- scvi/external/models/modules.py
+++ scvi/external/models/modules.py
@@ -25,10 +25,6 @@ class FCLayers(nn.Module):
         The dimensionality of the input
     n_out
         The dimensionality of the output
-    n_cat_list
-        A list containing, for each category of interest,
-        the number of categories. Each category will be
-        included using a one-hot encoding.
     n_layers
         The number of fully-connected hidden layers
     n_hidden
@@ -47,7 +43,6 @@ class FCLayers(nn.Module):
         self,
         n_in: int,
         n_out: int,
-        n_cat_list: Iterable[int] = None,
         n_layers: int = 1,
         n_hidden: int = 128,
         dropout_rate: float = 0.1,
@@ -58,19 +53,13 @@ class FCLayers(nn.Module):
         super().__init__()
         layers_dim = [n_in] + (n_layers - 1) * [n_hidden] + [n_out]
 
-        if n_cat_list is not None:
-            # n_cat = 1 will be ignored
-            self.n_cat_list = [n_cat if n_cat > 1 else 0 for n_cat in n_cat_list]
-        else:
-            self.n_cat_list = []
-
         self.fc_layers = nn.Sequential(
             collections.OrderedDict(
                 [
                     (
                         "Layer {}".format(i),
                         nn.Sequential(
-                            nn.Linear(n_in + sum(self.n_cat_list), n_out, bias=bias),
+                            nn.Linear(n_in, n_out, bias=bias),
                             # Below, 0.01 and 0.001 are the default values for `momentum` and `eps` from
                             # the tensorflow implementation of batch norm; we're using those settings
                             # here too so that the results match our old tensorflow code. The default
@@ -89,14 +78,12 @@ class FCLayers(nn.Module):
             )
         )
 
-    def forward(self, x: torch.Tensor, *cat_list: int, instance_id: int = 0):
+    def forward(self, x: torch.Tensor, instance_id: int = 0):
         """Forward computation on ``x``.
         Parameters
         ----------
         x
             tensor of values with shape ``(n_in,)``
-        cat_list
-            list of category membership(s) for this sample
         instance_id
             Use a specific conditional instance normalization (batchnorm)
         x: torch.Tensor
@@ -105,43 +92,11 @@ class FCLayers(nn.Module):
         py:class:`torch.Tensor`
             tensor of shape ``(n_out,)``
         """
-        one_hot_cat_list = []  # for generality in this list many indices useless.
-        assert len(self.n_cat_list) <= len(
-            cat_list
-        ), "nb. categorical args provided doesn't match init. params."
-        for n_cat, cat in zip(self.n_cat_list, cat_list):
-            assert not (
-                n_cat and cat is None
-            ), "cat not provided while n_cat != 0 in init. params."
-            if n_cat > 1:  # n_cat = 1 will be ignored - no additional information
-                if cat.size(1) != n_cat:
-                    one_hot_cat = one_hot(cat, n_cat)
-                else:
-                    one_hot_cat = cat  # cat has already been one_hot encoded
-                one_hot_cat_list += [one_hot_cat]
+
         for layers in self.fc_layers:
             for layer in layers:
                 if layer is not None:
-                    if isinstance(layer, nn.BatchNorm1d):
-                        if x.dim() == 3:
-                            x = torch.cat(
-                                [(layer(slice_x)).unsqueeze(0) for slice_x in x], dim=0
-                            )
-                        else:
-                            x = layer(x)
-                    else:
-                        if isinstance(layer, nn.Linear):
-                            if x.dim() == 3:
-                                one_hot_cat_list_layer = [
-                                    o.unsqueeze(0).expand(
-                                        (x.size(0), o.size(0), o.size(1))
-                                    )
-                                    for o in one_hot_cat_list
-                                ]
-                            else:
-                                one_hot_cat_list_layer = one_hot_cat_list
-                            x = torch.cat((x, *one_hot_cat_list_layer), dim=-1)
-                        x = layer(x)
+                    x = layer(x)
         return x
 
 
@@ -156,10 +111,6 @@ class Encoder(nn.Module):
         The dimensionality of the input (data space)
     n_output
         The dimensionality of the output (latent space)
-    n_cat_list
-        A list containing the number of categories
-        for each category of interest. Each category will be
-        included using a one-hot encoding
     n_layers
         The number of fully-connected hidden layers
     n_hidden
@@ -176,7 +127,6 @@ class Encoder(nn.Module):
         self,
         n_input: int,
         n_output: int,
-        n_cat_list: Iterable[int] = None,
         n_layers: int = 1,
         n_hidden: int = 128,
         dropout_rate: float = 0.1,
@@ -188,7 +138,6 @@ class Encoder(nn.Module):
         self.encoder = FCLayers(
             n_in=n_input,
             n_out=n_hidden,
-            n_cat_list=n_cat_list,
             n_layers=n_layers,
             n_hidden=n_hidden,
             dropout_rate=dropout_rate,
@@ -196,12 +145,7 @@ class Encoder(nn.Module):
         self.mean_encoder = nn.Linear(n_hidden, n_output)
         self.var_encoder = nn.Linear(n_hidden, n_output)
 
-        if distribution == "ln":
-            self.z_transformation = nn.Softmax(dim=-1)
-        else:
-            self.z_transformation = identity
-
-    def forward(self, x: torch.Tensor, *cat_list: int):
+    def forward(self, x: torch.Tensor):
         """The forward computation for a single sample.
 
          #. Encodes the data into latent space using the encoder network
@@ -222,10 +166,10 @@ class Encoder(nn.Module):
         """
 
         # Parameters for latent distribution
-        q = self.encoder(x, *cat_list)
+        q = self.encoder(x)
         q_m = self.mean_encoder(q)
         q_v = torch.exp(self.var_encoder(q)) + 1e-4
-        latent = self.z_transformation(reparameterize_gaussian(q_m, q_v))
+        latent = reparameterize_gaussian(q_m, q_v)
         return q_m, q_v, latent
 
 
@@ -240,10 +184,6 @@ class Decoder(nn.Module):
         The dimensionality of the input (latent space)
     n_output
         The dimensionality of the output (data space)
-    n_cat_list
-        A list containing the number of categories
-        for each category of interest. Each category will be
-        included using a one-hot encoding
     n_layers
         The number of fully-connected hidden layers
     n_hidden
@@ -259,7 +199,6 @@ class Decoder(nn.Module):
         self,
         n_input: int,
         n_output: int,
-        n_cat_list: Iterable[int] = None,
         n_layers: int = 1,
         n_hidden: int = 128,
     ):
@@ -267,7 +206,6 @@ class Decoder(nn.Module):
         self.px_decoder = FCLayers(
             n_in=n_input,
             n_out=n_hidden,
-            n_cat_list=n_cat_list,
             n_layers=n_layers,
             n_hidden=n_hidden,
             dropout_rate=0,
@@ -317,17 +255,14 @@ class LinearDecoder(nn.Module):
         self,
         n_input: int,
         n_output: int,
-        n_cat_list: Iterable[int] = None,
         use_batch_norm: bool = True,
         bias: bool = False,
     ):
-        super(LinearDecoderSCVI, self).__init__()
+        super().__init__()
 
-        # mean gamma
         self.factor_regressor = FCLayers(
             n_in=n_input,
             n_out=n_output,
-            n_cat_list=n_cat_list,
             n_layers=1,
             use_relu=False,
             use_batch_norm=use_batch_norm,
@@ -336,10 +271,10 @@ class LinearDecoder(nn.Module):
         )
 
     def forward(
-        self, dispersion: str, z: torch.Tensor, library: torch.Tensor, *cat_list: int
+        self, dispersion: str, z: torch.Tensor, library: torch.Tensor
     ):
         # The decoder returns values for the parameters of the NB/Poisson distribution
-        raw_px_scale = self.factor_regressor(z, *cat_list)
+        raw_px_scale = self.factor_regressor(z)
         px_scale = torch.softmax(raw_px_scale, dim=-1)
         px_rate = torch.exp(library) * px_scale
 
@@ -359,10 +294,6 @@ class GaussianDecoder(nn.Module):
         The dimensionality of the input (latent space)
     n_output
         The dimensionality of the output (data space)
-    n_cat_list
-        A list containing the number of categories
-        for each category of interest. Each category will be
-        included using a one-hot encoding
     n_layers
         The number of fully-connected hidden layers
     n_hidden
@@ -378,7 +309,6 @@ class GaussianDecoder(nn.Module):
         self,
         n_input: int,
         n_output: int,
-        n_cat_list: Iterable[int] = None,
         n_layers: int = 1,
         n_hidden: int = 128,
     ):
@@ -386,7 +316,6 @@ class GaussianDecoder(nn.Module):
         self.decoder = FCLayers(
             n_in=n_input,
             n_out=n_hidden,
-            n_cat_list=n_cat_list,
             n_layers=n_layers,
             n_hidden=n_hidden,
             dropout_rate=0,
@@ -395,7 +324,7 @@ class GaussianDecoder(nn.Module):
         self.mean_decoder = nn.Linear(n_hidden, n_output)
         self.var_decoder = nn.Linear(n_hidden, n_output)
 
-    def forward(self, x: torch.Tensor, *cat_list: int):
+    def forward(self, z: torch.Tensor):
         """The forward computation for a single sample.
 
          #. Decodes the data from the latent space using the decoder network
@@ -405,8 +334,6 @@ class GaussianDecoder(nn.Module):
         ----------
         x
             tensor with shape ``(n_input,)``
-        cat_list
-            list of category membership(s) for this sample
 
         Returns
         -------
@@ -414,10 +341,40 @@ class GaussianDecoder(nn.Module):
             Mean and variance tensors of shape ``(n_output,)``
 
         """
-        # Parameters for latent distribution
-        p = self.decoder(x, *cat_list)
+
+        # Parameters for Likelihood
+        p = self.decoder(z)
         p_m = self.mean_decoder(p)
-        p_v = torch.exp(self.var_decoder(p))
+        p_v = torch.exp(self.var_decoder(p)) + 1e-4
         return p_m, p_v
 
+class GaussianLinearDecoder(nn.Module, sigma):
+    def __init__(
+        self,
+        n_input: int,
+        n_output: int,
+        use_batch_norm: bool = True,
+        bias: bool = False,
+        sigma: float = sigma
+    ):
+        super().__init__()
 
+        self.factor_regressor = FCLayers(
+            n_in=n_input,
+            n_out=n_output,
+            n_layers=1,
+            use_relu=False,
+            use_batch_norm=use_batch_norm,
+            bias=bias,
+            dropout_rate=0,
+        )
+
+    def forward(
+        self, z: torch.Tensor
+    ):
+        # Mean of Normal
+        p_m = self.factor_regressor(z)
+        p_v = self.sigma * torch.ones(size=)
+        px_rate = torch.exp(library) * px_scale
+
+        return px_scale, px_rate, None
