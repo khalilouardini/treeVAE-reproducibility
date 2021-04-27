cadd00cf6e0152dbf26b7bda7283707889c9204f
Khalil Ouardini <khalil.ouardini@student.ecp.fr>
2021-04-27 01:50:34 -0700

    Adding the Gaussian VAE model

---
+++ scvi/external/inference/gaussian_inference.py
@@ -0,0 +1,242 @@
+import sys
+import logging
+import matplotlib.pyplot as plt
+from inference import Trainer, UnsupervisedTrainer
+from inference.posterior import Posterior
+from ..models.gaussian_vae import GaussianVAE
+from ..dataset.tree import GeneExpressionDataset
+import torch
+logger = logging.getLogger(__name__)
+
+plt.switch_backend("agg")
+
+
+class GaussianTrainer(Trainer):
+    r"""The VariationalInference class for the unsupervised training of an autoencoder
+    with a latent tree structure.
+
+	Args:
+		:model: A model instance from class ``GaussianVAE``
+		:gene_dataset: A Gene Expression Dataset
+		:train_size: The train size, either a float between 0 and 1 or an integer for the number of training samples
+		 to use Default: ``0.8``.
+		:test_size: The test size, either a float between 0 and 1 or an integer for the number of training samples
+		 to use Default: ``None``, which is equivalent to data not in the train set. If ``train_size`` and ``test_size``
+		 do not add to 1 or the length of the dataset then the remaining samples are added to a ``validation_set``.
+		:n_epochs_kl_warmup: Number of epochs for linear warmup of KL(q(z|x)||p(z)) term. After `n_epochs_kl_warmup`,
+			the training objective is the ELBO. This might be used to prevent inactivity of latent units, and/or to
+			improve clustering of latent space, as a long warmup turns the model into something more of an autoencoder.
+		:\*\*kwargs: Other keywords arguments from the general Trainer class.
+
+	Examples:
+		>>> gene_dataset = GeneExpressionDataset(X)
+        >>> vae = GaussianVAE(gene_dataset.nb_genes)
+        ... n_batch=tree_dataset.n_batches * use_batches, use_cuda=True)
+        >>> trainer = GaussianTrainer(vae, tree_dataset)
+        >>> trainer.train(n_epochs=400)
+	"""
+    default_metrics_to_monitor = ["elbo"]
+
+    def __init__(
+        self,
+        model,
+        gene_dataset,
+        lambda_ = 1.0,
+        train_size=0.8,
+        test_size=None,
+        n_epochs_kl_warmup=100,
+        **kwargs
+    ):
+
+        train_size = float(train_size)
+        if train_size > 1.0 or train_size <= 0.0:
+            raise ValueError(
+                "train_size needs to be greater than 0 and less than or equal to 1"
+            )
+        super().__init__(model, gene_dataset, **kwargs)
+
+        # Set up number of warmup iterations
+        self.n_epochs_kl_warmup = n_epochs_kl_warmup
+        self.normalize_loss = True
+
+        # Total size of the dataset used for training
+        self.n_samples = 1.0
+
+        self.train_set, self.test_set, self.validation_set =  self.train_test_validation(model=model,
+                                                                                         gene_dataset=gene_dataset,
+                                                                                         train_size=train_size,
+                                                                                         test_size=test_size,
+                                                                                         type_class=GaussianPosterior
+                                                                                         )
+            
+        self.train_set.to_monitor = ["elbo"]
+        self.test_set.to_monitor = ["elbo"]
+        self.validation_set.to_monitor = ["elbo"]
+        self.n_samples = len(self.train_set.indices)
+
+    @property
+    def posteriors_loop(self):
+        return ["train_set"]
+
+    def loss(self, tensors):
+        """ Computes the loss of the model after a specific iteration.
+
+        Computes the mean reconstruction loss, which is derived after a forward pass
+        of the model.
+
+        :param tensors: Observations to be passed through model
+
+        :return: Mean reconstruction loss
+        """
+        sample_batch, _, _, _, _ = tensors
+
+        reconst_loss, kl, _ = self.model.forward(x=sample_batch)
+        
+        loss = (
+            self.n_samples
+            * torch.mean(reconst_loss + self.kl_weight * kl)
+        )
+        if self.normalize_loss:
+            loss = loss / self.n_samples
+        return loss
+
+    def on_epoch_begin(self):
+        if self.n_epochs_kl_warmup is not None:
+            self.kl_weight = min(1, self.epoch / self.n_epochs_kl_warmup)
+        else:
+            self.kl_weight = 1.0
+
+    def __setattr__(self, name, value):
+        if isinstance(value, GaussianPosterior):
+            name = name.strip("_")
+            self.register_posterior(name, value)
+        else:
+            object.__setattr__(self, name, value)
+
+
+class GaussianPosterior(Posterior):
+    """
+    :param model: A model instance from class ``GaussianVAE``
+    :param gene_dataset: A gene_dataset instance from class ``GeneExpressionDataset``
+
+    """
+
+    def __init__(
+        self,
+        model: GaussianVAE,
+        gene_dataset: GeneExpressionDataset,
+        shuffle=False,
+        indices=None,
+        use_cuda=True,
+        data_loader_kwargs=dict(),
+    ):
+        super().__init__(
+            model=model,
+            gene_dataset=gene_dataset,
+            shuffle=shuffle,
+            indices=indices,
+            use_cuda=use_cuda,
+            data_loader_kwargs=data_loader_kwargs
+        )
+
+    def elbo(self) -> float:
+        elbo = self.compute_elbo(self.model)
+        logger.debug("ELBO : %.4f" % elbo)
+        return elbo
+
+    def compute_elbo(self, vae, **kwargs):
+        # Iterate once over the posterior and compute the elbo
+        print("computing elbo")
+        self.use_cuda = False
+
+        elbo = 0
+        for i_batch, tensors in enumerate(self):
+            sample_batch, _, _, _, _ = tensors[:5]
+            reconst_loss, kl, _ = vae.forward(sample_batch)
+            elbo += torch.sum(reconst_loss + kl).item()
+        n_samples = len(self.indices)
+        print("ELBO: {}".format(elbo))
+        return elbo / n_samples
+
+    @torch.no_grad()
+    def get_latent(self,
+             give_mean=False
+     ):
+        """Output posterior z mean or sample, batch index, and label
+
+        Parameters
+        ----------
+        sample
+            z mean or z sample
+        give_mean
+             (Default value = True)
+
+        Returns
+        -------
+        latent
+            low-dim representation
+        batch_indices
+            batch indicies corresponding to each cell
+        labels
+            label corresponding to each cell
+
+        """
+        latent = []
+        for tensors in self:
+            sample_batch, _, _, _, _ = tensors
+            latent += [
+                self.model.sample_from_posterior_z(
+                    sample_batch, give_mean=give_mean
+                ).cpu()
+            ]
+        return np.array(torch.cat(latent))
+
+    @torch.no_grad()
+    def generate(
+        self,
+        n_samples: int = 100,
+        batch_size: int = 128,
+    ):
+        """Create observation samples from the Posterior Predictive distribution
+
+        Parameters
+        ----------
+        n_samples
+            Number of required samples for each cell
+        genes
+            Indices of genes of interest
+        batch_size
+            Desired Batch size to generate data
+
+        Returns
+        -------
+        x_new : :py:class:`torch.Tensor`
+            tensor with shape (n_cells, n_genes, n_samples)
+        x_old : :py:class:`torch.Tensor`
+            tensor with shape (n_cells, n_genes)
+
+        """
+        x_old = []
+        x_new = []
+        for tensors in self.update({"batch_size": batch_size}):
+            sample_batch, _, _, _, _ = tensors
+            outputs = self.model.inference(sample_batch)
+
+            px_m = outputs["px_m"]
+            px_v = outputs["px_v"]
+            dist = Normal(qz_m, qz_v.sqrt())
+
+            gene_expressions = dist.sample() 
+
+            x_old.append(sample_batch.cpu())
+            x_new.append(gene_expressions.cpu())
+
+        x_old = torch.cat(x_old)
+        x_new = torch.cat(x_new)
+
+        return x_new.numpy(), x_old.numpy()
+
+    
+
+        
+
---
+++ scvi/external/models/gaussian_vae.py
@@ -0,0 +1,175 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Normal, kl_divergence
+
+from torch.distributions import Normal
+from ..models.modules import Encoder, GaussianDecoder, GaussianLinearDecoder
+
+from typing import Tuple, Dict
+
+torch.backends.cudnn.benchmark = True
+
+
+# Gaussian VAE model
+class GaussianVAE(nn.Module):
+    """Variational auto-encoder model.
+
+    Parameters
+    ----------
+    n_input
+        Number of input genes
+    n_labels
+        Number of labels
+    n_hidden
+        Number of nodes per hidden layer
+    n_latent
+        Dimensionality of the latent space
+    n_layers
+        Number of hidden layers used for encoder and decoder NNs
+    dropout_rate
+        Dropout rate for neural networks
+
+    """
+
+    def __init__(
+        self,
+        n_input: int,
+        n_batch: int = 0,
+        n_labels: int = 0,
+        n_hidden: int = 128,
+        n_latent: int = 10,
+        n_layers: int = 1,
+        dropout_rate: float = 0.1,
+        latent_distribution: str = "normal",
+        sigma_ldvae: float = None,
+    ):
+        super().__init__()
+        self.n_latent = n_latent
+        self.n_labels = n_labels
+        self.latent_distribution = latent_distribution
+
+        # z encoder goes from the n_input-dimensional data to an n_latent-d
+        # latent space representation
+        self.z_encoder = Encoder(
+            n_input,
+            n_latent,
+            n_layers=n_layers,
+            n_hidden=n_hidden,
+            dropout_rate=dropout_rate,
+            distribution=latent_distribution,
+        )
+
+        # decoder goes from n_latent-dimensional space to n_input-d data
+        if not sigma_ldvae:
+            self.decoder = GaussianDecoder(
+                n_latent,
+                n_input,
+                n_layers=n_layers,
+                n_hidden=n_hidden,
+            )
+        else:
+            self.decoder = GaussianLinearDecoder(
+                n_latent,
+                n_input,
+                n_layers=n_layers,
+                n_hidden=n_hidden,
+                sigma=sigma_ldvae
+            )
+
+    def get_latents(self, x):
+        """Returns the result of ``sample_from_posterior_z`` inside a list
+
+        Parameters
+        ----------
+        x
+            tensor of values with shape ``(batch_size, n_input)``
+
+        Returns
+        -------
+        type
+            one element list of tensor
+
+        """
+        return [self.sample_from_posterior_z(x)]
+
+    def sample_from_posterior_z(self, x, give_mean=False, n_samples=5000):
+        """Samples the tensor of latent values from the posterior
+
+        Parameters
+        ----------
+        x
+            tensor of values with shape ``(batch_size, n_input)``
+        give_mean
+            is True when we want the mean of the posterior  distribution rather than sampling (Default value = False)
+        n_samples
+            how many MC samples to average over for transformed mean (Default value = 5000)
+
+        Returns
+        -------
+        type
+            tensor of shape ``(batch_size, n_latent)``
+
+        """
+        qz_m, qz_v, z = self.z_encoder(x)
+        if not give_mean:
+            samples = Normal(qz_m, qz_v.sqrt()).sample([n_samples])
+            z = torch.mean(samples, dim=0)
+        else:
+            z = qz_m
+        return z
+
+    def inference(self, x, n_samples=1):
+        """Helper function used in forward pass"""
+        x_ = x
+        # Sampling
+        qz_m, qz_v, z = self.z_encoder(x_)
+
+        if n_samples > 1:
+            qz_m = qz_m.unsqueeze(0).expand((n_samples, qz_m.size(0), qz_m.size(1)))
+            qz_v = qz_v.unsqueeze(0).expand((n_samples, qz_v.size(0), qz_v.size(1)))
+            z = Normal(qz_m, qz_v.sqrt()).sample()
+
+        px_m, px_v = self.decoder(z)
+
+        return dict(
+            px_m=px_m,
+            px_v=px_v,
+            qz_m=qz_m,
+            qz_v=qz_v,
+            z=z
+        )
+
+    def forward(self, x):
+        """ Returns the reconstruction loss
+
+        :param x: tensor of values with shape (batch_size, n_input)
+
+        :return: the reconstruction loss and the Kullback divergences
+        :rtype: 2-tuple of :py:class:`torch.FloatTensor`
+        """
+        # Parameters for z latent distribution
+        outputs = self.inference(x)
+        qz_m = outputs["qz_m"]
+        qz_v = outputs["qz_v"]
+        px_m = outputs["px_m"]
+        px_v = outputs["px_v"]
+        z = outputs["z"]
+   
+        # KL divergence
+        mean = torch.zeros_like(qz_m)
+        scale = torch.ones_like(qz_v)
+
+        kl = kl_divergence(Normal(qz_m, torch.sqrt(qz_v)), Normal(mean, scale)).sum(dim=1)
+
+        # Reconstruction loss
+        reconst_loss = -Normal(px_m, torch.sqrt(px_v)).log_prob(x).sum(dim=-1)
+
+        return reconst_loss, kl, 0.0
+
+
+
+
+
+
